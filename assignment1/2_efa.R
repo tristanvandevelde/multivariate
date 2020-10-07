c316b478ddf3d61567445be87cf8f5e0a350ca59
Tristan Vandevelde <tristan@tristanvandevelde.io>
2020-10-07 12:05:04 +0200

    fix correlation plot

--- assignment1/2_efa.R
+++ assignment1/2_efa.R
@@ -1,4 +1,6 @@
 library(MASS)
+library(ggplot2)
+library(ggcorplot)
 #load("data/wvs.Rdata")
 load("/Users/tristanvandevelde/Documents/Dev/multivariate/assignment1/data/wvs.Rdata")
 
@@ -29,12 +31,25 @@ wvs_stand$J_violence <- scale(wvs_stand$J_violence, center = TRUE, scale = TRUE)
 #### EFA MODELLING
 ##################
 
-### STEP 1: FIT PCA TO DETERMINE AMOUNT OF FACTORS
 
-# print correlation matrix
+### STEP 0: SOME EXPLORATION
 cormat <- cor(wvs_stand[1:14])
 round(cormat,2)
-# intuitively: 3 factors (financial, sexual, violence)
+ggcorrplot(cormat)
+
+### STEP 1: FIT PCA TO DETERMINE AMOUNT OF FACTORS
+pca_just <- prcomp(wvs_stand[c(1:14)])
+screeplot(pca_just, type="lines")
+# both scree plot and kaisers criterium indicate te retain 3 facts
+# Consistent with our intuition (3 factors: financial, sexual, violence)
+
+
+
+### STEP 2: FIT EFA WITH 3 FACTORS
+fa_just <- factanal(~J_claiming_benefits+J_avoiding_fare+J_stealing_property+J_cheating_taxes+J_accept_bribe+J_homosexuality+J_prostitution+J_abortion+J_divorce+J_sex_before_marriage+J_suicide+J_beat_wife+J_parents_beating_children+J_violence, factors = 3, rotation="varimax", data=wvs_stand)
+
+
+
 #fa_just = <- factanal
 
 # safe factor scores and visualize for each factor the distribution for the 34 countries
