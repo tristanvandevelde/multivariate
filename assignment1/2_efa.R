792e77d5a17421461276c24f73bf793a46856458
Tristan Vandevelde <tristan@tristanvandevelde.io>
2020-10-17 11:46:50 +0200

    component selection

--- assignment1/2_efa.R
+++ assignment1/2_efa.R
@@ -38,13 +38,27 @@ cormat <- cor(justifiability_stand[1:14])
 round(cormat,2)
 ggcorrplot(cormat)
 
-### STEP 1: FIT PCA TO DETERMINE AMOUNT OF FACTORS
+### STEP 1A: FIT PCA TO DETERMINE AMOUNT OF FACTORS
 pca_justifiability <- prcomp(justifiability_stand[c(1:14)])
 screeplot(pca_justifiability, type="lines")
 abline(h=1, col="blue", lty=2)
 abline(v=4, col="red", lty=2)
 # both scree plot and kaisers criterium indicate te retain 3 facts
 # Consistent with our intuition (3 factors: financial, sexual, violence)
+### STEP 1B: COMPUTE PERCENTAGE OF NON-REDUNDANT RESIDUAL CORRELATIONS
+# 2 factors
+nonred_resid_corr <- function(f) {
+  efa_model <- factanal(justifiability_stand[1:14], factors=f)
+  resid <-cormat - (crossprod(t(efa_model$loadings))+diag(efa_model$uniquenesses))
+  n<-sum(ifelse(abs(resid)>0.05,1,0))/2
+  print(n/(14*13/2))
+}
+for (i in c(1:7))
+{
+  print(i)
+  nonred_resid_corr(i)
+}
+# 3 factors
 
 
 ### STEP 2: FIT MODELS
