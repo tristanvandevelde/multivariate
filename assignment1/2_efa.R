fd4e1b13ba42772c2b34f53ec62b06d24a2feb83
Tristan Vandevelde <tristan@tristanvandevelde.io>
2020-10-28 13:19:43 +0100

    smthsmth

--- assignment1/2_efa.R
+++ assignment1/2_efa.R
@@ -28,19 +28,21 @@ justifiability_stand$J_beat_wife <- scale(justifiability_stand$J_beat_wife, cent
 justifiability_stand$J_parents_beating_children <- scale(justifiability_stand$J_parents_beating_children, center = TRUE, scale = TRUE)
 justifiability_stand$J_violence <- scale(justifiability_stand$J_violence, center = TRUE, scale = TRUE)
 
+id <- rownames(justifiability_stand)
+justifiability_stand <- cbind(id=id, justifiability_stand)
 
 #### EFA MODELLING
 ##################
 
 
 ### STEP 0: SOME EXPLORATION
-cormat <- cor(justifiability_stand[1:14])
+cormat <- cor(justifiability_stand[2:15])
 round(cormat,2)
 ggcorrplot(cormat)
 
 ### STEP 1A: FIT PCA TO DETERMINE AMOUNT OF FACTORS
 # why does scree plot only go to 10, not to 14??
-pca_justifiability <- prcomp(justifiability_stand[c(1:14)])
+pca_justifiability <- prcomp(justifiability_stand[c(2:15)])
 screeplot(pca_justifiability, type="lines")
 abline(h=1, col="blue", lty=2)
 abline(v=4, col="red", lty=2)
@@ -48,7 +50,7 @@ abline(v=4, col="red", lty=2)
 # Consistent with our intuition (3 factors: financial, sexual, violence)
 ### STEP 1B: COMPUTE PERCENTAGE OF NON-REDUNDANT RESIDUAL CORRELATIONS
 nonred_resid_corr <- function(f) {
-  efa_model <- factanal(justifiability_stand[1:14], factors=f, rotation="none")
+  efa_model <- factanal(justifiability_stand[2:15], factors=f, rotation="none")
   resid <-cormat - (crossprod(t(efa_model$loadings))+diag(efa_model$uniquenesses))
   n<-sum(ifelse(abs(resid)>0.05,1,0))/2
   print(n/(14*13/2))
@@ -63,44 +65,44 @@ for (i in c(1:7))
 ### STEP 2: FIT MODELS
 ### NO ROTATION
 # no rotation 1
-efa_just_1_none <- factanal(justifiability_stand[1:14], factors=1)
+efa_just_1_none <- factanal(justifiability_stand[2:15], factors=1)
 efa_just_1_none
 # differs significantly from perfectly fitting model
 
 # no rotation 2
-efa_just_2_none <- factanal(justifiability_stand[1:14], factors=2)
+efa_just_2_none <- factanal(justifiability_stand[2:15], factors=2)
 efa_just_2_none
 # differs significantly from perfectly fitting model
 
 # no rotation 3
-efa_just_3_none <- factanal(justifiability_stand[1:14], factors=3)
+efa_just_3_none <- factanal(justifiability_stand[2:15], factors=3)
 efa_just_3_none
 # differs significantly from perfectly fitting model
 
 # no rotation 4
-efa_just_4_none <- factanal(justifiability_stand[1:14], factors=4)
+efa_just_4_none <- factanal(justifiability_stand[2:15], factors=4)
 efa_just_4_none
 # differs significantly from perfectly fitting model
 
 # no rotation 4
-efa_just_4_none <- factanal(justifiability_stand[1:14], factors=5)
+efa_just_4_none <- factanal(justifiability_stand[2:15], factors=5)
 efa_just_4_none
 # differs significantly from perfectly fitting model
 
 # no rotation 9
-efa_just_9_none <- factanal(justifiability_stand[1:14], factors=9)
+efa_just_9_none <- factanal(justifiability_stand[2:15], factors=9)
 efa_just_9_none
 # does not differ significantly from perfectly fitting model -> at last
 
 
 ### CHECK FOR ROTATION
 # orthogonal 3
-efa_just_3_orthogonal <- factanal(justifiability_stand[1:14], factors=3, rotation = "varimax")
+efa_just_3_orthogonal <- factanal(justifiability_stand[2:15], factors=3, rotation = "varimax")
 efa_just_3_orthogonal
 #efa_just_3_orthogonal$loadings[1:14,]
 
 # oblique 3 (we expect this to be the best model)
-efa_justifiability_3_oblique <- fa(justifiability_stand[1:14], 3, rotate="oblimin", fm="mle")
+efa_justifiability_3_oblique <- fa(justifiability_stand[2:15], 3, rotate="oblimin", fm="mle")
 efa_justifiability_3_oblique
 print(efa_justifiability_3_oblique, cutoff=0)
 efa_justifiability_3_oblique$loadings
@@ -139,27 +141,30 @@ abline(v=0)
 
 # COUNTRIES
 
-country_value_matrix <- aggregate(values_stand[,1:10], list(values_stand$country), mean)
-country_just_matrix <- aggregate(justifiability_stand[,1:14], list(justifiability_stand$country), mean)
-
+first_half = c("Armenia", "Australia")
 
 # save scores per car model and carid
-score<-cbind(na.omit(car)[,1:2],fact4obl$scores)
+score <- cbind(justifiability_stand[,c(1,16)],efa_justifiability_3_oblique$scores)
 # distribution Factor 1 per car model
-R> par(pty="s")
-R> par(las = 1,cex=1.2) # all axis labels horizontal R> par(oma=c(2,6,2,2)) # increase space for labels R> boxplot(score$ML1~score$carid,horizontal=TRUE,
-xlab="factor score",main="F1: fun to drive")
-# distribution Factor 2 per car model R> par(pty="s")
-R> par(las = 1,cex=1.2)
-R> par(oma=c(2,6,2,2))
-R> boxplot(score$ML2~score$carid,horizontal=TRUE,
-           xlab="factor score",main="F2: dependable, comfortable, safe")
+par(pty="s")
+par(las = 1,cex=1.2) # all axis labels horizontal 
+par(oma=c(2,6,2,2)) # increase space for labels 
+boxplot(score$ML1~score$country,horizontal=TRUE, xlab="factor score",main="F1: fun to drive")
+
+## distribution Factor 2 per car model 
+score <- score[order(-country),]
+#par(pty="s")
+#par(las = 2,cex=1.2)
+par(las=2)
+#par(oma=c(1,1,1,1))
+#par(mar=c(1,2,2,1))
+boxplot(score$ML2~score$country, horizontal=FALSE, xlab="", ylab="factor score", main="F2: dependable, comfortable, safe")
 
 # distribution Factor 3 per car model R> par(pty="s")
-R> par(las = 1,cex=1.2)
-R> par(oma=c(2,6,2,2))
-R> boxplot(score$ML3~score$carid,horizontal=TRUE, xlab="factor score",main="F3: suited for off road driving")
-# distribution Factor 4 per car model R> par(pty="s")
-R> par(las = 1,cex=1.2)
-R> par(oma=c(2,6,2,2))
-R> boxplot(score$ML4~score$carid,horizontal=TRUE, xlab="factor score",main="F4: family-oriented")
+#R> par(las = 1,cex=1.2)
+#R> par(oma=c(2,6,2,2))
+#R> boxplot(score$ML3~score$carid,horizontal=TRUE, xlab="factor score",main="F3: suited for off road driving")
+## distribution Factor 4 per car model R> par(pty="s")
+#R> par(las = 1,cex=1.2)
+#R> par(oma=c(2,6,2,2))
+#R> boxplot(score$ML4~score$carid,horizontal=TRUE, xlab="factor score",main="F4: family-oriented")
--- assignment1/4_sem.R
+++ assignment1/4_sem.R
@@ -1,39 +1,51 @@
 # ---Assignment 1---
 # - Question 4 - 
-#setwd("C:\\Users\\altyn\\Desktop\\KU Leuven\\Multivariate Statistics\\Assignment")
-#load("wvs.Rdata")
-load("/Users/tristanvandevelde/Documents/Dev/multivariate/assignment1/data/wvs.Rdata")
-
-library("dbplyr")
-library("tidyverse")
-library("MASS")
-library("heplots")
+setwd("C:\\Users\\altyn\\Desktop\\KU Leuven\\Multivariate Statistics\\Assignment")
+load("wvs.Rdata")
 library("lavaan")
-sem1 <- '
-  #measurement model
-  justification =~1*J_claiming_benefits + J_stealing_property + J_cheating_taxes + J_accept_bribe + J_homosexuality + J_prostitution + J_abortion + J_divorce + J_sex_before_marriage + J_suicide+ J_beat_wife + J_parents_beating_children + J_violence
-  religion =~1*R_attend_religious_services + R_pray + R_importance_God
+library("dplyr")
+
+#database for Malaysia
+Malaysia <- filter(wvs[,16:33], country=="Malaysia")
+Malaysia <- Malaysia[,1:17]
+
+#database for Netherlands
+Neth <- filter(wvs[,16:33], country=="Netherlands")
+Neth <- Neth[,1:17]
+
+
+#building the model
+model.sem <- '#measurement model
+  sex =~ J_homosexuality + J_prostitution + J_abortion + J_divorce + 
+  J_sex_before_marriage
+  fraud =~ J_claiming_benefits + J_avoiding_fare + J_stealing_property + 
+  J_cheating_taxes + J_accept_bribe
+  violence =~ J_beat_wife + J_parents_beating_children + J_violence
+  suicide =~J_suicide
+  religion =~ NA*R_attend_religious_services + R_pray + R_importance_God
   
   #structural model
-  justification ~~ religion
-  
-  #variances latent variables
-  justification ~~ justification
-  religion ~~ 1*religion'
+  religion ~~ sex + fraud + violence + suicide
 
-#Netherlands
-Neth <- filter(wvs[,16:33], country=="Netherlands")
-#estimate model
-fitsem1<- sem(sem1, Neth[,1:17])
-summary(fitsem1, fit.measure=T)
-standardizedSolution(fitsem1)
+  #variances of latent variables
+  religion ~~ 1*religion
+ '
+#check the model fitting for the Netherlands
+sem.Neth <- sem(model.sem, data = Neth)
+summary(sem.Neth, standardized = TRUE)
+fitmeasures(sem.Neth, c("chisq","df","pvalue","cfi","tli","rmsea","srmr"))
 
-#Malasia
-Malas <- filter(wvs[,16:33], country=="Malaysia")
-#estimate model 
-fitsem2<- sem(sem1, Malas[,1:17])
-summary(fitsem2, fit.measures=T)
-standardizedSolution(fitsem2)
+#checking for indices to improve
+modificationindices(sem.Neth, sort=TRUE)
+
+
+#check the model fitting for Malaysia
+sem.Mal <- sem(model.sem, data = Malaysia)
+summary(sem.Mal, standardized = TRUE)
+fitmeasures(sem.Mal, c("chisq","df","pvalue","cfi","tli","rmsea","srmr"))
+
+#checking for indices to improve
+modificationindices(sem.Mal, sort=TRUE)
 
 
 
